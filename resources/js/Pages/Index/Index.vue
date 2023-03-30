a88c24645c7945699bc6c8e8de84e53478afbb56
worawut <worawut.w32@gmail,com>
2023-03-31 06:52:54 +0700

    add laravel project to github

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Index</div>
+  <Link href="/show">show Page</Link>
+  <div>message is {{message}}</div>
+</template>
+
+<script setup>
+
+import { Link } from '@inertiajs/vue3'
+defineProps(['message'])
+
+
+</script>
+
+<style>
+
+</style>
