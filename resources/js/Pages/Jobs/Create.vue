421a94831b2fe3f1fdaed2e17c8ca64fc6b49839
worawut <worawut.w32@gmail,com>
2023-04-12 07:47:41 +0700

    edit create form

--- resources/js/Pages/Jobs/Create.vue
+++ resources/js/Pages/Jobs/Create.vue
@@ -1,69 +1,69 @@
 <template>
   <form @submit.prevent="create">
-    <div>
-      <div>
-        <label>Job</label>
-        <input v-model="form.job_name" type="text" />
+    <div class="grid grid-cols-6 gap-4">
+      <div class="col-span-2">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Job</label>
+        <input v-model="form.job_name" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
         <div v-if="form.errors.job_name">
           {{form.errors.job_name}}
         </div>
       </div>
 
-      <div>
-        <label>Company</label>
-        <input v-model="form.company" type="text" />
+      <div class="col-span-2">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Company</label>
+        <input v-model="form.company" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
         <div v-if="form.errors.company">
           {{form.errors.company}}
         </div>
       </div>
 
-      <div>
-        <label>Area</label>
-        <input v-model="form.area" type="text" />
+      <div class="col-span-2">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Area</label>
+        <input v-model="form.area" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
         <div v-if="form.errors.area">
           {{form.errors.area}}
         </div>
       </div>
 
-      <div>
-        <label>Postcode</label>
-        <input v-model="form.post_code" type="text" />
+      <div class="col-span-2">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Postcode</label>
+        <input v-model="form.post_code" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
          <div v-if="form.errors.post_code">
           {{form.errors.post_code}}
         </div>
       </div>
-      <div>
-        <label>city</label>
-        <input v-model="form.city" type="text" />
+      <div class="col-span-4">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">city</label>
+        <input v-model="form.city" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
         <div v-if="form.errors.city">
           {{form.errors.city}}
         </div>
       </div>
-        <div>
-        <label>Details</label>
-        <input v-model="form.detail" type="text" />
+        <div class="col-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Details</label>
+        <input v-model="form.detail" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
         <div v-if="form.errors.detail">
           {{form.errors.detail}}
         </div>
       </div>
-      <div>
-        <label>skills</label>
-        <input v-model="form.skills" type="text" />
+      <div class="cal-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">skills</label>
+        <input v-model="form.skills" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
         <div v-if="form.errors.skills">
           {{form.errors.skills}}
         </div>
       </div>
 
-      <div>
-        <label>salary</label>
-        <input v-model.number="form.salary" type="number"/>
+      <div class="cal-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">salary</label>
+        <input v-model.number="form.salary" type="number" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"/>
            <div v-if="form.errors.salary">
           {{form.errors.salary}}
         </div>
       </div>
 
-      <div>
-        <button type="submit">Create</button>
+      <div class="col-span-6">
+        <button type="submit" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">Create</button>
       </div>
     </div>
   </form>
--- resources/js/Pages/Jobs/Show.vue
+++ resources/js/Pages/Jobs/Show.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="grid grid-cols-12 gap-4 ">
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4 ">
         <Box class="col-span-12 md:col-span-7 flex items-center w-full">
             <div class="w-full text-center font-medium">Image</div>
         </Box>
+        <div class="md:col-span-5 flex flex-col gap-4">
         <Box class="col-span-12 md:col-span-5">
+            <template #header>
+                Details
+            </template>
        <JobSpace :job="job" class="text-gray-500" />
        <JobListing :job="job" class="text-gray-500" />
        <Salary :salary="job.salary" class="text-2xl font-bold"/>
         </Box>
+        <Box>
+            <template #header>
+                Save
+            </template>
+            Apply Job
+        </Box>
+        </div>
     </div>
 </template>
 
